d6a863371bb01a2a75f4bb028810faefbe4ec968
(Apple) Xuanchen Li <apple20010209@163.com>
2022-05-31 14:30:53 -0700

    try to commit

--- toolkit/tools/boilerplate/boilerplate.go
+++ toolkit/tools/boilerplate/boilerplate.go
@@ -7,10 +7,12 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/boilerplate/hello"
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/exe"
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/logger"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/timestamp"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -23,6 +25,7 @@ var (
 )
 
 func main() {
+	defer timestamp.Track(time.Now(), "BoilerPlate")
 	app.Version(exe.ToolkitVersion)
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
