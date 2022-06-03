67c1f39f502f534de652301dc0e7c9d551207a3a
(Apple) Xuanchen Li <apple20010209@163.com>
2022-06-03 16:59:29 -0700

    fixed bug, testcases in test file works now.

--- toolkit/tools/internal/timestamp/timestamp_test.go
+++ toolkit/tools/internal/timestamp/timestamp_test.go
@@ -11,14 +11,28 @@ import (
 
 //TestMain found in configuration_test.go.
 
-func Test_WritetoFile_Range_Instant(t *testing.T) {
+func Test_WritetoFile_range_instant(t *testing.T) {
 	TrackToFile(time.Now(), "test tool", "test step", true, os.Stdout)
 }
 
-func Test_WritetoFile_noRange_Instant(t *testing.T) {
+func Test_WritetoFile_noRange_instant(t *testing.T) {
 	TrackToFile(time.Now(), "test tool", "test step", false, os.Stdout)
 }
 
-func Test_WritetoCSV_Range_Instant(t *testing.T) {
+func Test_WritetoCSV_range_instant(t *testing.T) {
 	TrackToCSV(time.Now(), "test tool", "test step", true)
+}
+
+func Test_WritetoCSV_noRange_instant(t *testing.T) {
+	TrackToCSV(time.Now(), "test tool", "test step", false)
+}
+
+func Test_WritetoFile_range_sleeps(t *testing.T) {
+	defer TrackToFile(time.Now(), "tool 1", "step 1", true, os.Stdout)
+	time.Sleep(3 * time.Second)
+}
+
+func Test_WritetoCSV_range_sleeps(t *testing.T) {
+	defer TrackToCSV(time.Now(), "test tool", "test step", true)
+	time.Sleep(3 * time.Second)
 }
