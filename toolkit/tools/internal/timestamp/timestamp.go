864914ddad5a7dad928601a2fff8cdb247bae9ab
(Apple) Xuanchen Li <apple20010209@163.com>
2022-06-03 11:28:44 -0700

    created struct, wrapper function not done yet

--- toolkit/tools/internal/timestamp/timestamp.go
+++ toolkit/tools/internal/timestamp/timestamp.go
@@ -1,22 +1,29 @@
 package timestamp
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"time"
 )
 
 type timeInfo struct {
-	toolName  string // Name of the tool
-	stepName  string // Name of the step
-	duration  string // Time to complete the step (ms)
-	start     string // Start time of the step
-	end       string // End time for the step
-	timeRange bool   // Whether to record start and end time
+	toolName	string	// Name of the tool
+	stepName	string	// Name of the step
+	duration	int		// Time to complete the step (ms)
+	start		string	// Start time of the step
+	end		string		// End time for the step
+	timeRange	bool	// Whether to record start and end time
 }
 
-// var (
-// 	data = []timeInfo{}
-// )
+var(
+	// data = [][]string{
+	// 	{"Tool Name", "Step Name", "Duration", "Start", "End"},
+	// }
+	data = []timeInfo{}
+)
 
 // Call at the begining of the main() of a tool using "defer timestamp(time.Now(), "name_of_function")".
 // The tool needs to import "time" too.
@@ -30,57 +37,29 @@ func Track(start time.Time, name string) string {
 
 // output as a string
 // make a class output io.Writer
-func TrackToFile(start time.Time, toolName string, stepName string, timeRange bool, writer io.Writer) {
-	curr := track(start, toolName, stepName, timeRange)
-	msg := "Step " + stepName + " in " + toolName + " took " + curr.duration + ". "
-	if timeRange {
-		msg += "Started at " + curr.start + "; ended at " + curr.end + ". \n"
-	} else {
-		msg += "\n"
-	}
-	_, err := io.WriteString(writer, msg)
+func TrackToFile(start time.Time, name string, writer io.Writer) {
+	msg := track(start, name)
+	n, err := io.WriteString(writer, msg)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Wrote %d bytes\n", n)
 
 }
 
+// features: initialize timestamp, flag each run (start & end, wipe out? ),
 // go tool for csv files (for future parsing), tool name, step name, time, flag for time range
 func TrackToCSV(start time.Time, toolName string, stepName string, timeRange bool) {
-	//	Read text to rewrite later
-	a, err := ioutil.ReadFile("build-time.csv") // the file is inside the local directory
-	content := string(a)
+	file, err := os.Create("build-time.csv")
 	if err != nil {
-		fmt.Printf("Fail to read to file. %s\n", err)
-	}
-	fmt.Println(content)
-
-	// Create a new .csv file.
-	file, err := os.Create("build-time.csv") // this step will be moved to the init stage later
-	if err != nil {
-		fmt.Printf("Failed to create the csv file. %s\n", err)
+		fmt.Println("Failed to create the csv file", err)
 	}
 	defer file.Close()
 
-	// Create a new csv writer.
 	writer := csv.NewWriter(file)
-	defer writer.Flush()
+	defer writer.flush()
 
-	// Run
-	curr := track(start, toolName, stepName, timeRange)
-	if timeRange {
-		err = writer.Write([]string{curr.toolName, curr.stepName, curr.duration, curr.start, curr.end})
-		writer.Write([]string{content})
-		fmt.Println([]string{content})
-	} else {
-		err = writer.Write([]string{curr.toolName, curr.stepName, curr.duration})
-		writer.Write([]string{content})
-	}
-	if err != nil {
-		fmt.Printf("Fail to write to file. %s\n", err)
-	}
 }
-
 // output sth in the trace level?
 // figure out logger package (how to call logger everywhere without passing a parameter)
 
