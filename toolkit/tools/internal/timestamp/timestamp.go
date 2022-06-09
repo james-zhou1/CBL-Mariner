b36df6e6609fec95b314b2130c69690826629c0b
james-zhou1 <james@james-Virtual-Machine>
2022-06-09 16:13:02 -0400

    remove commented out code

--- toolkit/tools/internal/timestamp/timestamp.go
+++ toolkit/tools/internal/timestamp/timestamp.go
@@ -43,7 +43,6 @@ func TrackToFile(start time.Time, toolName string, stepName string, timeRange bo
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Println("Wrote %d bytes\n", n)
 
 }
 
@@ -71,7 +70,6 @@ func TrackToCSV(start time.Time, toolName string, stepName string, timeRange boo
 	// Run
 	curr := track(start, toolName, stepName, timeRange)
 	if timeRange {
-		// bytesWritten, err := writer.WriteString(curr.toolName + curr.stepName + curr.duration + curr.start + curr.end)
 		err = writer.Write([]string{curr.toolName, curr.stepName, curr.duration, curr.start, curr.end})
 		writer.Write([]string{content})
 	} else {
