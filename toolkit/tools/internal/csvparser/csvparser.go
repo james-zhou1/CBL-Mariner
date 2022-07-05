710988f774411e5ae129fd4a650c9eac3b170a45
james-zhou1 <james@james-Virtual-Machine>
2022-07-05 15:06:13 -0400

    store csv outputs in array

--- toolkit/tools/internal/csvparser/csvparser.go
+++ toolkit/tools/internal/csvparser/csvparser.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
+
+var timeArray [][]string
 
 // Reads a CSV file, and returns data to the terminal
 func ParseAndExport(filename string) {
@@ -19,23 +22,25 @@ func ParseAndExport(filename string) {
 	fileScanner := bufio.NewScanner(file)
 
 	for fileScanner.Scan() {
-		fmt.Println((fileScanner.Text()))
+		timeArray = append(timeArray, strings.Split(fileScanner.Text(), ","))
 	}
 
 }
 
 func ParseCSV() {
 
-	create_worker_chroot_path := "/home/james/repos/CBL-Mariner/toolkit/tools/internal/timestamp/results/create_worker_chroot.csv"
+	// create_worker_chroot_path := "/home/james/repos/CBL-Mariner/toolkit/tools/internal/timestamp/results/create_worker_chroot.csv"
 	image_config_validator_path := "/home/james/repos/CBL-Mariner/toolkit/tools/internal/timestamp/results/imageconfigvalidator.csv"
 	image_pkg_fetcher_path := "/home/james/repos/CBL-Mariner/toolkit/tools/internal/timestamp/results/imagepkgfetcher.csv"
 	imager_path := "/home/james/repos/CBL-Mariner/toolkit/tools/internal/timestamp/results/imager.csv"
 	roast_path := "/home/james/repos/CBL-Mariner/toolkit/tools/internal/timestamp/results/roast.csv"
-	ParseAndExport(create_worker_chroot_path)
+	// ParseAndExport(create_worker_chroot_path)
 	ParseAndExport(image_config_validator_path)
 	ParseAndExport(image_pkg_fetcher_path)
 	ParseAndExport(imager_path)
 	ParseAndExport(roast_path)
+
+	fmt.Printf("%v\n", timeArray)
 
 	startTime, err := time.Parse(time.RFC1123, "Fri, 01 Jul 2022 17:56:44 EDT")
 	if err != nil {
