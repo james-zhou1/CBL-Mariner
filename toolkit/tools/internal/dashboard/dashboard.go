ab368568124144342b60aac0b5768349a712b3bd
james-zhou1 <james@james-Virtual-Machine>
2022-07-12 12:50:52 -0400

    a

--- toolkit/tools/internal/dashboard/dashboard.go
+++ toolkit/tools/internal/dashboard/dashboard.go
@@ -20,49 +20,35 @@ var (
 		"imager.csv":               []int64{0, 4},
 		"roast.csv":                []int64{0, 3},
 	}
-	// targetCSV = []string{"create_worker_chroot.csv", "imageconfigvalidator.csv", "imagepkgfetcher.csv", "imager.csv", "roast.csv"}
-	// CSVSize   = []int64{0, 0, 0, 0, 0}
-	// targetJSON = []string{}
 )
 
 func main() {
 	fmt.Println("Starting dashboard")
-	uiprogress.Start() // start rendering
-	bar := uiprogress.AddBar(22)
-	bar.AppendCompleted()
-	bar.PrependElapsed()
+	uiprogress.Start()
+	bar := uiprogress.AddBar(22).AppendCompleted().PrependElapsed()
 
 	wd, _ := os.Getwd()
 	idx := strings.Index(wd, "CBL-Mariner/toolkit")
 	wd = wd[0 : idx+19]
 	targetDir = wd + "/tools/internal/timestamp/results/"
 
-	// Use an infinite for loop to watch out for new updates
 	for {
-		// run this iteration periodically for smaller overhead
 		time.Sleep(1 * time.Second)
-
-		// Check if the target directory exists. Assume we only need to check one directory for now.
-		currStat, err := os.Stat(targetDir)
+		_, err := os.Stat(targetDir)
 		if os.IsNotExist(err) {
 			continue
 		}
-
-		// Check update for each target file.
 		for filePath, _ := range targetCSV {
-			currStat, err = os.Stat(targetDir + filePath)
-			// Check if the file exists.
+			_, err = os.Stat(targetDir + filePath)
 			if os.IsNotExist(err) {
 				continue
 			}
-			// getUpdate(currStat, idx, filePath)
 			currNumLines := getNumLines(targetDir + filePath)
 			if currNumLines != targetCSV[filePath][0] {
 				targetCSV[filePath][0] = currNumLines
-				fmt.Printf("%s has %d lines \n", currStat.Name(), currNumLines)
+				// fmt.Printf("%s has %d lines \n", currStat.Name(), currNumLines)
 				bar.Incr()
 			}
-			// getUpdate(currStat, idx, filePath)
 		}
 	}
 
